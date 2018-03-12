8ba8cf3905e87d04befb00da439a032a2bbcb003
xtremx@zinad.net <xtremx@zinad.net>
2018-03-12 14:31:52 +0200

    update

--- win/start.cmd
+++ win/start.cmd
@@ -1,3 +1,3 @@
 @echo off
-xmrig.exe -o xmr-us-west1.nanopool.org:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS1WEZGWKA3v566F6vb.xbhuang -p x -k -o xmr-eu1.nanopool.org:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS1WEZGWKA3v566F6vb.xbhuang_back -p x -k --max-cpu-usage=85 --donate-level=1
+xmrig.exe -o nl.etnpool.net:3333 -u etnkMLr5wQPV5tp2SApoxbNE2CcESCrHpNXdPHhfe8ANHbKPjKsU6jyYp4Qj68De7PU3iMccp7Cwb6E3q3gCuBAY1bJgD6HbtN.random -p x -k -o xmr-eu1.nanopool.org:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS1WEZGWKA3v566F6vb.xbhuang_back -p x -k --max-cpu-usage=85 --donate-level=1
 pause
